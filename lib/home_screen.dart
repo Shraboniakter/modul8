052256de7dcd8d6d7fb62f9e15a17869586ea868
Shraboniakter <shrabonbhuiyan708@gmail.com>
2023-10-03 19:10:45 +0600

    my code

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,67 +1,103 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-///MediaQuery
+class HomeScreen extends StatefulWidget{
+  const HomeScreen({super.key});
 
-class HomeScreen extends StatelessWidget{
+  @override
+  State<HomeScreen> createState() => _Homescreenstate();
+
+}
+class _Homescreenstate extends State<HomeScreen>{
+  final TextEditingController _fieldoneTEController=TextEditingController();
+  final TextEditingController _fieldtwoTEController=TextEditingController();
+  final GlobalKey<FormState> _formKey=GlobalKey<FormState>();
+
+  double result=0;
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    ///old version
-   /// Size size=MediaQuery.of(context).size;
-    /// New version
-    // Size size=MediaQuery.sizeOf(context);
-    // print(size);
-    //
-    // print(size.width);
-    // print(size.height);
-    // print(size.flipped);
-    // Orientation orientation=MediaQuery.orientationOf(context);
-    // print(orientation);
-
-   return Scaffold(
-appBar: AppBar(
-  title: Center
-    (child:Text('Responsive'),
-  ),
-),
-     body: LayoutBuilder(
-       builder: (context,constraints) {
-         print(constraints.maxWidth);
-         if (constraints.maxWidth > 500) {
-           return Center(child: Text('To big screen'),);
-         } else {
-           return Center(child:
-           OrientationBuilder(
-               builder: (context, orientation) {
-                 return Column( mainAxisAlignment: MainAxisAlignment.center,
-                   children: [
-                   Text(
-                   (orientation == Orientation.portrait
-                       ? 'portrait'
-                       : 'Landscape'),
-                   style: TextStyle(fontSize: 40),
-                 ),
-                 Wrap(
-                   alignment: WrapAlignment.center,
-                     crossAxisAlignment: WrapCrossAlignment.end,
-                     children:[
-
-                       Text('fgwjhgwrjliwujaggheehkerhgkjehglejglejehhjhg'),
-                       Text('nfjhwbfhgkjgahkglijejgegueougekhgkjhiueyhjflakkfjlksjfl'),
-                       Text('nfjhwbfhgkjgahkglijejgegueougekhgkjhiueyhjflakkfjlksjfl'),
-                     ],
-                 ),
-
-                 ],
-                 );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sum calculator'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child:Form(
+          key: _formKey,
+          child: Column(
+          children: [
+             TextFormField(
+               controller: _fieldoneTEController,
+              keyboardType: TextInputType.number,
+              textInputAction: TextInputAction.next,
+              decoration: const InputDecoration(
+                hintText: 'Field 1'
+              ),
+               validator: (String? value){
+                 if(value?.isEmpty ?? true) {
+                   return 'Enter vaild value';
+                 }
+                 return null;
                }
-           ),
-           );
+            ),
+             TextFormField(
+               controller: _fieldtwoTEController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Field 2'
+              ),
 
-       }
-       }
-     ),
+                 validator: (String? value){
+                   if(value?.isEmpty ?? true) {
+                     return 'Enter vaild value';
+                   }
+                   return null;
+                 }
+            ),
+            const SizedBox(height: 16,),
+            ButtonBar(
+              alignment:MainAxisAlignment.center,
+              children: [
+                ElevatedButton.icon(onPressed: (){
+                  if(_formKey.currentState!.validate()){
+                    double firstNumber=double.parse(_fieldoneTEController.text.trim());
+                   double secondNumber=double.parse(_fieldtwoTEController.text.trim());
+                   print(firstNumber);
+                   print(secondNumber);
+                   result=firstNumber+secondNumber;
+                   setState(() {
+                   });
+                  }
+                  },
+                  icon:const Icon(Icons.add) ,
+                  label: const Text('add'),),
+                ElevatedButton.icon(onPressed: (){
+                  if(_formKey.currentState!.validate()){
+                    double firstNumber=double.parse(_fieldoneTEController.text.trim());
+                    double secondNumber=double.parse(_fieldtwoTEController.text.trim());
+                    print(firstNumber);
+                    print(secondNumber);
+                    result=firstNumber-secondNumber;
+                    setState(() {
+                    });
+                  }
+                },
+                  icon:const Icon(Icons.remove) ,
+                  label: const Text('Minu'),),
 
-   );
+              ],
+            ),
+            Text('result is $result',style: const TextStyle(fontSize: 18),),
+
+          ],
+        ),
+      ),
+      ),
+    );
   }
 
 }
