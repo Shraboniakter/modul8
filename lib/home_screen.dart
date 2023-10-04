d729d494d180029df0bf37e16bd63216b1ab57b3
Shraboniakter <shrabonbhuiyan708@gmail.com>
2023-10-04 22:21:52 +0600

    stack

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,106 +1,51 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _Homescreenstate();
-
-}
-class _Homescreenstate extends State<HomeScreen>{
-  final TextEditingController _fieldoneTEController=TextEditingController();
-  final TextEditingController _fieldtwoTEController=TextEditingController();
-  final GlobalKey<FormState> _formKey=GlobalKey<FormState>();
-
-  double result=0;
-
+class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sum calculator'),
+        title:
+          Text('Profile'),
+
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child:Form(
-          key: _formKey,
-          child: Column(
-          children: [
-             TextFormField(
-               controller: _fieldoneTEController,
-              keyboardType: TextInputType.number,
-               textInputAction: TextInputAction.next,
-               decoration: InputDecoration(
-                 hintText: 'Field 1'
-               ),
+     body: Center(child:
+         Stack(
+           children: [
+             Container(
+               width: 200,
+               height:200,
+               color: Colors.purple,
+             ),
+            Positioned(
+              bottom: 50,
+              right: 50,
+              child: Container(
+               width: 100,
+               height:100,
+
+               color: Colors.cyan,
+             ),
             ),
-            TextFormField(
-               controller: _fieldtwoTEController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: 'Field 2'
-              ),
-
-                 validator: (String? value){
-                   if(value?.isEmpty ?? true) {
-                     return 'Enter vaild value';
-                   }
-                   return null;
-                 }
+            Positioned.fill(
+              child:Align(
+                alignment: Alignment.topCenter,
+                child:
+              Container(
+               width: 50,
+               height:50,
+               color: Colors.pinkAccent,
+             ),
             ),
-            const SizedBox(height: 16,),
-            ButtonBar(
-              alignment:MainAxisAlignment.center,
-              children: [
-                ElevatedButton.icon(onPressed: (){
-                  if(_formKey.currentState!.validate()){
-                    double firstNumber=double.parse(_fieldoneTEController.text.trim());
-                   double secondNumber=double.parse(_fieldtwoTEController.text.trim());
-                   print(firstNumber);
-                   print(secondNumber);
-                   result=firstNumber+secondNumber;
-                   setState(() {
-                   });
-                  }
-                  },
-                  icon:const Icon(Icons.add) ,
-                  label: const Text('add'),),
-                ElevatedButton.icon(onPressed: (){
-                  if(_formKey.currentState!.validate()){
-                    double firstNumber=double.parse(_fieldoneTEController.text.trim());
-                    double secondNumber=double.parse(_fieldtwoTEController.text.trim());
-                    print(firstNumber);
-                    print(secondNumber);
-                    result=firstNumber-secondNumber;
-                    setState(() {
-                    });
-                  }
-                },
-                  icon:const Icon(Icons.remove) ,
-                  label: const Text('Minu'),),
-
-
-                ElevatedButton.icon(onPressed: (){
-                  if(_formKey.currentState!.validate()){
-                    double firstNumber=double.parse(_fieldoneTEController.text.trim());
-                    double secondNumber=double.parse(_fieldtwoTEController.text.trim());
-                    print(firstNumber);
-                    print(secondNumber);
-                    result=firstNumber*secondNumber;
-                    setState(() {
-                    });
-                  }
-                },
-                  icon:const Icon(Icons.multiple_stop) ,
-                  label: const Text('multiple'),),
-              ],
             ),
-            Text('result is $result',style: const TextStyle(fontSize: 18),),
 
-          ],
-        ),
-      ),
-      ),
+
+
+           ],
+         ),
+       ),
+
     );
   }
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 import 'package:modul8/home_screen.dart';
-import 'package:sizer/sizer.dart';
+
 
 void main() {
-  runApp(DevicePreview(
-      enabled: kDebugMode,
-      builder: (context) {
-        return MyApp();
-      },
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -20,15 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Sizer(
-        builder: (context, orientation, deviceType) {
-          print(deviceType);
+
           return MaterialApp(
-            locale: DevicePreview.locale(context),
-            builder: DevicePreview.appBuilder,
             home: HomeScreen(),
           );
         }
-    );
+
   }
-}
